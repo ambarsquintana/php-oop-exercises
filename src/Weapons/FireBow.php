bc70ab4436b7323aa495e328fb397f81f95e63fd
Ambar Quintana <ambarsquintana@gmail.com>
2020-01-08 02:09:34 -0400

    changes value magical in Weapon FireBow

--- src/Weapons/FireBow.php
+++ src/Weapons/FireBow.php
@@ -10,6 +10,6 @@ use Styde\Weapon;
 class FireBow extends Weapon
 {
     protected $damage = 30;
-    protected $magical = 1;
+    protected $magical = true;
     protected $description = ':unit dispara una flecha de fuego a :opponent';
 }

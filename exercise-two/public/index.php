828bb1683ec0b2f14e30a611148f58ef7d523558
Ambar Quintana <ambarsquintana@gmail.com>
2020-01-14 14:10:21 -0400

    magic methods __get, __set, __isset and __unset

--- exercise-two/public/index.php
+++ exercise-two/public/index.php
@@ -2,5 +2,32 @@
 
 require '../vendor/autoload.php';
 
+use Styde\User;
 
 
+$user = new User();
+
+$user->setAttributes([
+    'first_name' => 'Ambar',
+    'last_name'  => 'Quintana',   
+]);
+
+$user->nickname ='ambarsquintana';
+
+///////////////////////////////////////////////
+
+echo "<p>Bienvenido {$user->first_name} {$user->last_name}</p>";    
+
+if (isset($user->nickname)) {
+    echo "<p>Nickname: {$user->nickname}</p>";
+}
+
+var_dump($user->getAttributes());
+
+//////////////////////////////////////////////
+
+unset($user->nickname);
+
+echo "<br><br>";
+
+var_dump($user->getAttributes());
